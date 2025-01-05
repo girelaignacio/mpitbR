041f6e565b9002f6bcb10f1d14a3c7c87c8d3178
Ignacio Girela <girelaignacio@gmail.com>
2025-01-05 16:40:04 -0300

    add Benin's DHS surveys in data

--- R/data.R
+++ R/data.R
@@ -7,3 +7,29 @@
 #' @author Nicolai Suppa
 #' @references \doi{10.1177/1536867X231195286}
 "syn_cdta"
+
+#' Preprocessed Benin DHS 06 dataset
+#'
+#' The Benin DHS 06 dataset containing preprocessed data using the OPHI do-file from the Demographic and Health Survey (DHS) program.
+#'
+#' @name ben_dhs06
+#' @docType data
+#' @author OPHI
+#' @source
+#'  - DHS Program: https://ohss.dhs.gov/about-our-data
+#'  - OPHI do-file: https://ophi.org.uk/global-mpi/2024
+#' @references OPHI and UNDP (2024). Global Multidimensional Poverty Index 2024: Poverty amid conflict. United Nations Development Programme (UNDP) and Oxford Poverty and Human Development Initiative (OPHI), University of Oxford.
+"ben_dhs06"
+
+#' Preprocessed Benin DHS 17-18 dataset
+#'
+#' The Benin DHS 17-18 dataset containing preprocessed data using the OPHI do-file from the Demographic and Health Survey (DHS) program.
+#'
+#' @name ben_dhs17_18
+#' @docType data
+#' @author OPHI
+#' @source
+#'  - DHS Program: https://ohss.dhs.gov/about-our-data
+#'  - OPHI do-file: https://ophi.org.uk/global-mpi/2024
+#' @references OPHI and UNDP (2024). Global Multidimensional Poverty Index 2024: Poverty amid conflict. United Nations Development Programme (UNDP) and Oxford Poverty and Human Development Initiative (OPHI), University of Oxford.
+"ben_dhs17_18"
